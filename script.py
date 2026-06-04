223b41c7d3b4840cda52605c61696f61086b5213
Braulio de jesus Medina Macias <medinamasisbrauliodejesus@gmail.com>
2026-06-03 20:42:50 -0600

    Se agrega script.py que imprime nombre y comandos favoritos

---
+++ script.py
@@ -0,0 +1,8 @@
+# Script de Python - Examen Parcial
+nombre = "Braulio de jesus medina macias"
+comandos_favoritos = ["git checkout", "git status", "git push"]
+
+print(f"Nombre: {Braulio}")
+print("Mis 3 comandos de Git favoritos son:")
+for comando in comandos_favoritos:
+    print(f"- {git add,git init, git push}")
